cd4119702bbc33384965bb002d3441270b7716de
margaretmm <myq927@126.com>
2020-05-18 08:02:57 +0800

    Create KmeansInputData.java

---
+++ src/main/java/com/dataartisans/flinktraining/KmeansInputData.java
@@ -0,0 +1,39 @@
+public class KmeansInputData {
+    private String v1;
+    private String v2;
+    private String v3;
+    private String groupbyId;
+
+    public String getGroupbyId() {
+        return groupbyId;
+    }
+
+    public void setGroupbyId(String groupbyId) {
+        this.groupbyId = groupbyId;
+    }
+
+
+    public String getV1() {
+        return v1;
+    }
+
+    public void setV1(String v1) {
+        this.v1 = v1;
+    }
+
+    public String getV2() {
+        return v2;
+    }
+
+    public void setV2(String v2) {
+        this.v2 = v2;
+    }
+
+    public String getV3() {
+        return v3;
+    }
+
+    public void setV3(String v3) {
+        this.v3 = v3;
+    }
+}
