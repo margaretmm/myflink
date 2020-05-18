46f05b68541e83d4873f851be36851a16d891cd3
margaretmm <myq927@126.com>
2020-05-18 08:00:27 +0800

    Create DistantCompute.java

---
+++ src/main/java/com/dataartisans/flinktraining/DistantCompute.java
@@ -0,0 +1,19 @@
+public class DistantCompute {
+
+    public double getEuclideanDis(Point p1,Point p2){
+        double count_dis=0;
+        float[] p1_local_array=p1.getLocalArray();
+        float[] p2_local_array=p2.getLocalArray();
+
+        if(p1_local_array.length!= p2_local_array.length){
+            throw new IllegalArgumentException("length of array must be equel!");
+        }
+
+        for(int i=0;i<p1_local_array.length;i++){
+            count_dis+=Math.pow(p1_local_array[i]-p2_local_array[i],2);
+        }
+
+        return Math.sqrt(count_dis);
+    }
+
+}
